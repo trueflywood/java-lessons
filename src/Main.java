1942f0b5f23769bded2780484fe99fc18c93cdd3
Алексей Компанец <trueflywood@gmail.com>
2023-03-11 20:04:38 +0300

    Задание №4. Циклы

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         printPiramid();
@@ -6,16 +8,10 @@ public class Main {
         System.out.println("\n");
         printTriangleTwo();
         System.out.println("\n");
-        printSlash();
-        System.out.println("\n");
-        printBackSlash();
-        System.out.println("\n");
         printRomb();
         System.out.println("\n");
         printSquare();
         System.out.println("\n");
-        printOctagon();
-
     }
 
     /**
@@ -27,26 +23,53 @@ public class Main {
      * Символ отвечающий за фигуру
      */
     static String star = "*";
-
+    static Scanner scanner = new Scanner(System.in);
     /**
      * Печать равностороннего треугольника
      */
     public static void printPiramid() {
-        int line = 8;
+        System.out.println("введите высоту пирамиды");
+        final int line = scanner.nextInt();
+
 
         for (int i = 0; i < line; i++) {
             System.out.println(empty.repeat(line - i - 1) + star.repeat(1 + (i * 2)));
         }
+        System.out.println("\n");
+
+        // NOTE контур
+        for (int i = 0; i < line; i++) {
+            if(i == 0 || i == line - 1) {
+                System.out.println(empty.repeat(line - i - 1) + star.repeat(1 + (i * 2)));
+            } else {
+                System.out.println(empty.repeat(line - i - 1) + star + empty.repeat(1 + (i * 2) - 2) + star) ;
+            }
+
+        }
+
     }
 
     /**
      * Печать прямоугольного треугольника
      */
     public static void printTriangleOne() {
-        int line = 7;
+        System.out.println("введите высоту треугольника");
+        final int line = scanner.nextInt();
 
         for (int i = 0; i < line; i++) {
             System.out.println(star.repeat(1 + i));
+        }
+
+        System.out.println("\n");
+
+        // NOTE контур
+        for (int i = 0; i < line; i++) {
+            if(i < 2 || i == line - 1) {
+                System.out.println(star.repeat(1 + i));
+            } else {
+                System.out.println(star + empty.repeat(1 + i -2) + star);
+            }
+
         }
     }
 
@@ -54,40 +77,38 @@ public class Main {
      * Печать прямоугольного треугольника 2
      */
     public static void printTriangleTwo() {
-        int line = 7;
+        System.out.println("введите высоту треугольника");
+        final int line = scanner.nextInt();
 
         for (int i = 0; i < line; i++) {
             System.out.println(star.repeat(line - i));
         }
-    }
 
-    /**
-     * печать символа слеша
-     */
-    public static void printSlash() {
-        int line = 8;
+        System.out.println("\n");
 
+        // NOTE контур
         for (int i = 0; i < line; i++) {
-            System.out.println(empty.repeat(line - i - 1) + star);
+            if(i == 0 || i >= line - 2) {
+                System.out.println(star.repeat(line - i));
+            } else {
+                System.out.println(star + empty.repeat(line - i - 2) + star);
+            }
+
         }
     }
 
-    /**
-     * Печать символа обратного слеша
-     */
-    public static void printBackSlash() {
-        int line = 8;
-
-        for (int i = 0; i < line; i++) {
-            System.out.println(empty.repeat(i) + star);
-        }
-    }
 
     /**
      * Печать ромбика
      */
     public static void printRomb() {
-        int line = 9;
+
+        String empty = "   ";
+
+        String star = " * ";
+
+        System.out.println("введите длинну стороны ромба");
+        final int line = scanner.nextInt();
 
         for (int i = 0; i < line; i++) {
             System.out.println(empty.repeat(line - i - 1) + star.repeat(1 + (i * 2)));
@@ -95,40 +116,55 @@ public class Main {
         for (int i = line-1; i > 0; i--) {
             System.out.println(empty.repeat(line - i) + star.repeat( (i * 2) - 1));
         }
+
+        System.out.println("\n");
+
+        // NOTE контур
+        for (int i = 0; i < line; i++) {
+
+            if(i == 0 ) {
+                System.out.println(empty.repeat(line - i - 1) + star.repeat(1 + (i * 2)));
+            } else {
+                System.out.println(empty.repeat(line - i - 1) + star + empty.repeat(1 + (i * 2) - 2) + star) ;
+            }
+        }
+        for (int i = line-1; i > 0; i--) {
+            if (i == 1) {
+                System.out.println(empty.repeat(line - i) + star.repeat( (i * 2) - 1));
+            } else {
+                System.out.println(empty.repeat(line - i ) + star + empty.repeat((i * 2) - 3) + star) ;
+            }
+        }
+
+
     }
 
     /**
      * Печать прямоугольника
      */
     public static void printSquare() {
-        int line = 8;
-        int rows = 5;
+        String empty = "   ";
+
+        String star = " * ";
+        System.out.println("введите высоту прямоугольника");
+        final int line = scanner.nextInt();
+
+        System.out.println("введите ширину прямоугольника");
+        final int rows = scanner.nextInt();
 
         for (int i = 0; i < line; i++) {
-            System.out.println((star + empty).repeat(rows));
+            System.out.println((star).repeat(rows));
         }
+        System.out.println("\n");
 
-    }
-
-    /**
-     * Печать "круга"
-     */
-    public static void printOctagon() {
-
-        String text = """
-             * *
-      *               *
-   *                     *   
-
-*                           *
-*                           *
-
-   *                     *
-      *               *
-             * *
-            """;
-
-        System.out.print(text);
+        // NOTE контур
+        for (int i = 0; i < line; i++) {
+            if (i == 0 || i == line -1) {
+                System.out.println((star).repeat(rows));
+            } else {
+                System.out.println(star + (empty).repeat(rows -2) + star);
+            }
+        }
     }
 
 }
