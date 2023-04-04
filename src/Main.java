d342d15056432174b00eb39029298c523d15c989
Алексей Компанец <trueflywood@gmail.com>
2023-04-04 11:02:58 +0300

    Методы массива через while()

--- src/Main.java
+++ src/Main.java
@@ -17,23 +17,28 @@ public class Main {
         arr.print();
         System.out.println();
         System.out.println("Удаляем элемент с 3 индексом");
-        arr.removeByIndex(3);
+        arr.remove(3);
         arr.print();
         System.out.println("новый размер массива = " + arr.lenght());
         System.out.println();
         System.out.println("Добавляем значения по индексу");
         System.out.println("значение 5 по индексу 3 ");
-        arr.insertByIndex(3,5);
+        arr.add(3,5);
         arr.print();
         System.out.println("новый размер массива = " + arr.lenght());
         System.out.println();
         System.out.println("значение 56 по индексу 7 ");
-        arr.insertByIndex(7,56);
+        arr.add(7,56);
         arr.print();
         System.out.println("новый размер массива = " + arr.lenght());
         System.out.println();
         System.out.println("значение 22 по индексу 9 ");
-        arr.insertByIndex(9,22);
+        arr.add(9,22);
+        arr.print();
+        System.out.println("новый размер массива = " + arr.lenght());
+        System.out.println();
+        System.out.println("добавляем значение 33 в начало");
+        arr.addStart(33);
         arr.print();
         System.out.println("новый размер массива = " + arr.lenght());
     }
@@ -52,9 +57,9 @@ class MyArray {
     int[] arr;
 
     {
-        size = 10;
+        size = 3;
         index = 0;
-        arr = new int[10];
+        arr = new int[this.size];
     }
     MyArray() {}
 
@@ -64,61 +69,100 @@ class MyArray {
     }
     public void add(int num) {
         if (this.index == this.size) {
-            this.resizeArray();
+            this.regeneration();
         }
         this.arr[index++] = num;
     }
     public  void print(){
         System.out.print('[');
 
-        for (int i = 0; i < this.lenght(); i++) {
+//        for (int i = 0; i < this.lenght(); i++) {
+//            System.out.print(this.arr[i]);
+//            if (i != this.lenght()-1) System.out.print(",");
+//        }
+        int i = 0;
+        while (i < this.lenght()) {
             System.out.print(this.arr[i]);
             if (i != this.lenght()-1) System.out.print(",");
+            i++;
         }
         System.out.print(']');
     }
 
-    private void resizeArray() {
+    private void regeneration() {
         int[] newArr = new int[this.size * 2];
-        for (int i = 0; i < this.size; i++) {
+
+//        for (int i = 0; i < this.size; i++) {
+//            newArr[i] = this.arr[i];
+//        }
+        int i = 0;
+        while(i < this.size) {
             newArr[i] = this.arr[i];
+            i++;
         }
         this.size *= 2;
         this.arr = newArr;
     }
 
-    public void removeByIndex(int index) {
+    public void remove(int index) {
         if (index < this.index && index >= 0) {
             int[] newArr = new int[this.size];
-            for (int i = 0; i < this.index; i++) {
+
+//            for (int i = 0; i < this.index; i++) {
+//                if (index > i) {
+//                    newArr[i] = arr[i];
+//                } else if (index < i) {
+//                    newArr[i-1] = arr[i];
+//                }
+//            }
+
+            int i = 0;
+            while (i < this.index) {
                 if (index > i) {
                     newArr[i] = arr[i];
                 } else if (index < i) {
                     newArr[i-1] = arr[i];
                 }
+                i++;
             }
-            this.index = this.index- 1;
+            this.index = this.index - 1;
             this.arr = newArr;
         }
     }
 
-    public void insertByIndex(int index, int value) {
+    public void add(int index, int value) {
         if (this.index == this.size) {
-            this.resizeArray();
+            this.regeneration();
         }
         if (index < this.index && index >= 0) {
 
             int[] newArr = new int[this.size];
             newArr[index] = value;
-            for (int i = 0; i < this.index; i++) {
+
+//            for (int i = 0; i < this.index; i++) {
+//                if (index > i) {
+//                    newArr[i] = arr[i];
+//                } else if (index <= i) {
+//                    newArr[i+1] = arr[i];
+//                }
+//            }
+
+            int i = 0;
+            while (i < this.index) {
                 if (index > i) {
                     newArr[i] = arr[i];
                 } else if (index <= i) {
                     newArr[i+1] = arr[i];
                 }
+                i++;
             }
+
             this.index++;
             this.arr = newArr;
         }
+    }
+
+    public void addStart(int value) {
+        this.add(0, value);
     }
 }
