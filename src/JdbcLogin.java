e9e6b3e850083eb37765cee8a6ab3a9987f8313e
Алексей Компанец <trueflywood@gmail.com>
2023-07-01 10:33:55 +0300

    sql запросы

---
+++ src/JdbcLogin.java
@@ -0,0 +1,55 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcLogin {
+    private Connection connection;
+
+    public void StartBdd() throws ClassNotFoundException, SQLException {
+
+        String driverName = "com.mysql.cj.jdbc.Driver";
+        Class.forName(driverName); // here is the ClassNotFoundException
+
+
+        String serverName = "flywood.com6.ru";
+        String portNumber = "3306";
+        String mydatabase = "flywood_api";
+        String url = "jdbc:mysql://" + serverName + ":" + portNumber + "/" + mydatabase;
+
+
+        String password = "flykomp111";
+        String userName = "flywood_test_user";
+        this.connection = DriverManager.getConnection(url, userName, password);
+
+    }
+
+    public void close() throws SQLException {
+        this.connection.close();
+    }
+
+    public void execAndShow(String title, String sql) throws SQLException {
+        System.out.println();
+        System.out.println("Задание: "+ title);
+        System.out.println("Запрос: "+ sql);
+        System.out.println("Результат (только номер и название): ");
+
+        ResultSet res = this.connection.createStatement().executeQuery(sql);
+        // System.out.println(res.getArray(0));
+        int count = 0;
+
+        System.out.println("№  -  Название");
+        System.out.println();
+        while (res.next()) {
+            count++;
+            int id = res.getInt("N");
+            String str1 = res.getString("Name");
+            System.out.println(id + " - " + str1);
+        }
+        System.out.println();
+        System.out.println("всего: " + count);
+
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+import java.sql.SQLException;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+       JdbcLogin base = new JdbcLogin();
+        try {
+            base.StartBdd();
+            base.execAndShow(
+                    "1)Вывести топ 5 книг с наибольшем тиражом",
+                    "SELECT * FROM `dbo.books` order by Pressrun desc limit 5; "
+            );
+            base.execAndShow(
+                    "2)Вывести книги где цена больше 10 и меньше 50",
+                    "select * from `dbo.books` where (price >10 and price < 50)"
+            );
+            base.execAndShow(
+                    "3) Вывести книги где N коде меньше цены",
+                    "select * from `dbo.books` where price > N"
+            );
+            base.execAndShow(
+                    "4) Вывести книги где N коде больше тиража",
+                    "select * from `dbo.books` where N > Pressrun");
+            base.execAndShow(
+                    "5) Вывести книги которые занимают в списке 10-15 место по общей сумме (тираж * цена) ",
+                    "select *, (price * Pressrun) as sum from `dbo.books` order by sum desc limit 10,5");
+            base.execAndShow(
+                    "6)Вывести 2 процента старых книг",
+                    "SELECT * FROM( SELECT *, @counter := @counter +1 AS counter FROM (select @counter:=0) AS initvar, `dbo.books` where date not like '' ORDER BY Date asc ) AS X where counter <= (2/100 * @counter)");
+            base.execAndShow(
+                    "7)Вывести книги которые были выпущены с 1996 года по 1998",
+                    "select * from `dbo.books`  where year(date) >= 1996 and year(date) <= 1998 order by date");
+            base.execAndShow(
+                    "8)Вывести книги которые были выпущены в мае или в июле",
+                    "select * from `dbo.books`  where month(date) = 5 or month(date) = 7 ");
+            base.execAndShow(
+                    "9)Вывести топ 5 новых книг которые были выпущены зимой в отсортированном виде по цене (убывание)",
+                    "select * from (select * from `dbo.books` where  month(date) = 12 or month(date) = 1 or month(date) = 2 order by Date desc limit 5) as x order by Price desc");
+
+
+            base.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("ошибка подключения");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
     }
+
+
 }
+
+
